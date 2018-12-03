f6759ca489ea272069d48c1f9850a329d4522d3a
Andrew Adler <gleeb101@gmail.com>
2018-12-03 07:38:30 -0900

    Honestly I forgot a lot of the things I added - adding comments now

--- interface.cpp
+++ interface.cpp
@@ -1,63 +1,93 @@
-//Liam Eberhart
-//Andrew Adler
-//CS 303 Project 2
-//This is loosely based on the "Kingdom" series, mostly just resouce management
+// Liam Eberhart, Andrew Adler
+// interface.cpp
+// CS 301 Project 2: Resource Management Game
+// This is the C++ code for the Resource Management Game
+// linked with town.asm
 
 #include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
+#include <iomanip>
+using std::setw;
+using std::setfill;
 #include <string>
 using std::string;
 using std::getline;
+#include <cstdlib>
+using std::rand;
+using std::srand;
+#include <ctime>
+using std::time;
 #include <stdlib.h>
 using std::system;
 
 #define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
 
-extern "C" void testingFunction();
-
 extern "C" char * buildVillage();
 extern "C" void printVillage(char *);
 
-extern "C" void recruitVillager();
+extern "C" int recruitVillager();
 extern "C" int getVillagers();
 extern "C" int getVillagersAvail();
 
 extern "C" int buildFarm(char *);
 
 extern "C" int buildWall(char *);
+extern "C" int getWalls();
 
-extern "C" void moveTime();
-extern "C" void addFunds();
+extern "C" void moveTime(char *);
+extern "C" int getTime();
+extern "C" int getAttackChance();
+extern "C" int attack(char *);
 extern "C" int getFunds();
 
 int main () {
-    char *villagePtr = buildVillage();
+    char *villagePtr = buildVillage();  // Sets up "village" at runtime
     bool keepPlaying = true;
-    int line = 9;           // What line to move cursor to
-    int charSpaces = 3;     // How many chars to move cursor
-    int errorMessage = 0;
-    int linesUntilVillage = 0;
-    while (keepPlaying) {
-        system("clear");
-        line = 9;
-        charSpaces = 3;
+    int line;                           // What line to move cursor to
+    int charSpaces;                     // How many chars to move cursor
+    int message = 0;                    // Interface message to display
+    int linesUntilVillage;
+
+    srand(time(NULL));                  // Seeding the random number generator
+    int attackChance;                   // Calculated chance for attack each hour
+    int maxRandValue = 100;             // Used in calculating chance for attack
+    
+    while (keepPlaying) {   // Loops while the person continues playing
+        system("clear");    // Clears console for each loop (only on Linux)
+        line = 10;          // Lines down to set cursor to for input
+        charSpaces = 3;     // Characters to move cursor to the right
         linesUntilVillage = 6;
+
+        // Print the current time
+        cout << "Time: " << setw(2) << setfill('0') << getTime() << ":00";
+        if (getTime() <= 5) // Between 00:00 and 06:00, there is a chance to be attacked
+        {
+            cout << "\t (" << getAttackChance() << "\% chance of attack)";
+        }
+        cout << endl;
+
         cout << "Number of villagers: " << getVillagers() 
              << " (" << getVillagersAvail() << " unassigned)" << endl;
         cout << "Coin: " << getFunds() << endl;
 
-        cout << "Choose your action:\ne \t exit\nblank \t skip time\nr \t recruit villager\nf \t build a farm ("<<"#" << "//2)\nw \t walls\n";
+        cout << "Choose your action:\ne \t exit\n";
+        cout << "blank \t skip time\n";
+        cout << "r \t recruit villager\n";
+        cout << "f \t build a farm\n";
+        cout << "w \t build a wall\n";
 
-        if (errorMessage)
+        // If there is an additional message to display to user
+        //      Used for displaying specific error messages as well
+        if (message)
         {
             cout << endl;
 
-            switch (errorMessage)
+            switch (message)
             {
                 case 1:
-                    cout << "Input not recognized. Please enter a valid input\n";
+                    cout << "Input not recognized. Please enter a valid input.\n";
                     break;
 
                 case 2:
@@ -67,6 +97,26 @@ int main () {
                 case 3:
                     cout << "Not enough farmland to build another farm.\n";
                     break;
+
+                case 4:
+                    cout << "You've been attacked!\n";
+                    break;
+
+                case 5:
+                    cout << "Your unprotected village was attacked!" << endl;
+                    cout << "Game over..." << endl;
+                    line++;
+                    keepPlaying = false;
+                    continue;
+                    break;
+
+                case 6:
+                    cout << "You don't have enough funds to perform that action." << endl;
+                    break;
+
+                case 7:
+                    cout << "Your walls are already at their maximum level." << endl;
+                    break;
             }
 
             cout << endl;
@@ -74,11 +124,13 @@ int main () {
             line += 3;
             linesUntilVillage -= 3;
 
-            errorMessage = 0;
+            message = 0;
         }
 
-        cout << ">:";
+        cout << ">:";   // What is displayed in front of cursor accepting input
 
+        // To keep the village from moving from its location on the screen
+        // linesUntilVillage is updated when additional console messages are displayed
         for (int i = 0; i < linesUntilVillage; i++) 
         {
             cout << endl;
@@ -88,48 +140,75 @@ int main () {
         gotoxy(line,charSpaces);        // Move cursor to the ">:" to get input
 	    string input;
 	    getline(cin, input);
+
         if (input == "e") 
         {
             keepPlaying = false;
-	        cout << "Farewell!" << endl;
             system("clear");
+	        cout << "Farewell!" << endl;
             break;
         }
 	
-	else if(input == "" )
-	{
-	    moveTime();
-	    continue;
-	}
+        else if (input == "")
+        {
+            // Do nothing
+        }
 
         else if (input == "r") 
         {
-            recruitVillager();
-	}
+            if(message = recruitVillager())   
+            // If recruitVillager doesn't return 0, a villager was not recruited
+            {
+                continue;   // Prevents time from advancing
+            }
+	    }
         
-	else if (input == "f") 
+	    else if (input == "f") 
         {
-            //build a farm here, would give income limit farms?
-            if(errorMessage = buildFarm(villagePtr))   
+            // Build a farm here
+            if(message = buildFarm(villagePtr))   
             // If buildFarm doesn't return 0, a farm was not built
             {
-                continue;
+                continue;   // Prevents time from advancing
             }
-	}
+	    }
 
         else if (input == "w") 
         {
-            //build walls here, maybe have upgrade table/cost of upgrades?
-            buildWall(villagePtr);
-	}
+            // Build walls here
+            if (message = buildWall(villagePtr))
+            // If buildWall doesn't return 0, a wall was not built
+            {
+                continue;   // Prevents time from advancing
+            }
+	    }
 
-        else        // When an unexpected input is given
+        else                // When an unexpected input is given
         {
-            errorMessage = 1;
-            continue;
+            message = 1;
+            continue;       // Prevents time from advancing
         }
 
-        moveTime();
+        // Calculating if the village gets attacked
+        if (getTime() >= 0 && getTime() <= 5)
+        {
+            attackChance = rand()%maxRandValue + 1;
+            // attackChance will be a value 1-100
+            if (attackChance <= getAttackChance())
+            {
+                if (message = attack(villagePtr))
+                {
+                    // message = 5: game over message
+                }
+                else 
+                {
+                    // message = 4: player was attacked, but survived
+                    message = 4;
+                }
+            }
+        }
+
+        moveTime(villagePtr);   // After handling input, advance time by 1 hour
     }
     return 0;
 }
--- town.asm
+++ town.asm
@@ -1,4 +1,8 @@
-global testingFunction
+; Liam Eberhart, Andrew Adler
+; town.asm
+; CS 301 Project 2: Resource Management Game
+; This is the assembly code for the Resource Management Game
+; linked with interface.cpp
 
 global buildVillage
 global printVillage
@@ -10,134 +14,287 @@ global getVillagersAvail
 global buildFarm
 
 global buildWall
+global getWalls
 
 global moveTime
-global addFunds
+global getTime
+global getAttackChance
+global attack
 global getFunds
 
 extern printf
-
-testingFunction:
-    
-ret
+extern malloc
 
 ; **************** Village Functions ********************
+; buildVillage generates villagePtr and farmPtr
 buildVillage:
-    mov edi,[villageSize]    ; Village size (in bytes)
-    extern malloc
-    call malloc
+    mov edi,DWORD[villageSize]      ; Village size (in bytes)
+    push edi                        ; Pass value to malloc
+    call malloc                     ; Generate villagePtr
+    add esp,4                       ; Clear pushed value
+    push eax                        ; Save villagePtr until return
 
-    mov ecx, 0
-    fillVillage:
+    mov ecx, 0                      ; Loop counter
+    fillVillage:                    ; Initialized each spot of village with '_'
         mov BYTE[eax + ecx],'_'
         add ecx,1
-        cmp ecx,[villageSize]
+        cmp ecx,DWORD[villageSize]
         jl fillVillage
+
+    mov edi,DWORD[villageSize]      ; Total possible village size
+    sub edi, 2                      ; Subtract 2 for both walls
+    mov DWORD[numPossibleFarms],edi ; The remaining village spaces can be farms
+
+    makeFarmPtr:
+        mov edi,[numPossibleFarms]
+        imul edi,4                  ; Size of one int per farm
+        push edi                    ; Pass value to malloc
+        call malloc                 ; Allocate memory for the farm pointer
+        add esp,4                   ; Clear pushed value
+        mov DWORD[farmPtr],eax      ; Store the farm pointer
+
+    mov ecx,0
+    mov edx,DWORD[timeToMakeFarm]
+    sub edx, 1
+    fillFarmPtr:
+        mov eax,DWORD[farmPtr]      ; Get pointer to farm pointer
+        mov DWORD[eax+ecx*4],edx    ; Set each farm's value to -4
+
+        add ecx,1                   ; Progress loop
+        cmp ecx,DWORD[numPossibleFarms]
+        jne fillFarmPtr
+
+    returnVillage:
+    pop eax                         ; Return the village pointer
 ret
 
+; Displays the current status of the village
 printVillage:
-    mov eax, [esp+4] ; village pointer
-    mov ecx, 0
+    mov eax, [esp+4]                ; Village pointer
+
+    mov ecx, 0                      ; Loop counter
     startLoop:
-        push eax
+        push eax                    ; Preserve registers
         push ecx
 
-        push DWORD[eax]
-        push villageFormat
+        push DWORD[eax]             ; The next char in villagePtr
+        push villageFormat          ; The format string for printf
         call printf
-        add esp,8
+        add esp,8                   ; Clear stack after function call
 
         pop ecx
         pop eax
-        add eax, 1
+        add eax, 1                  ; Update variables for next loop
         add ecx, 1
         cmp ecx,DWORD[villageSize]
         jl startLoop
     
-    push printNewLine
+    push printNewLine               ; Prints a new line after the village
     call printf
-    push printNewLine
-    call printf
-    add esp,8
+    add esp,4
 ret
 
 ; **************** Villager Functions ********************
+; Adds a villager to be employed at a farm
 recruitVillager: 
-    add DWORD[villagers],1
-    add DWORD[villagers+4],1
-    sub DWORD[funds],1
+    cmp DWORD[funds],1              ; Check for sufficient funds
+    mov eax,DWORD[notEnoughFunds]
+    jl noRecruitedVillagers
+
+    mov eax,0                       ; Return 0 for successfully added villager
+    add DWORD[villagers],1          ; Add to number of villagers
+    add DWORD[villagers+4],1        ; Add to number of available villagers
+    sub DWORD[funds],1              ; Cost of recruiting a villager
+    noRecruitedVillagers:
 ret
 
+; Returns the current number of villagers
 getVillagers:
     mov eax, DWORD[villagers]
 ret
 
+; Returns the current number of available villagers
 getVillagersAvail:
     mov eax, DWORD[villagers + 4]
 ret
 
 ; **************** Farm Functions ********************
 buildFarm:
-    mov eax, [esp+4] ; village pointer
+    mov eax, [esp+4]                ; village pointer
 
-    cmp DWORD[villagers + 4],0
+    cmp DWORD[villagers + 4],0      ; Check for available villagers
     jg farmSuccess
 
-    mov eax,DWORD[notEnoughVillagers]               ; Error message = 2
+    mov eax,DWORD[notEnoughVillagers]           ; Error message = 2
     jmp return
 
     farmSuccess:
         mov ecx, 0
-        findFarm:                       ; Find next free space in village
-            cmp BYTE[eax + ecx],'_'
+        findFarm:                   ; Find next free space in village
+            cmp ecx,0
+            je tryNextFarmSpot      ; Can't build farm on first space (for a wall)
+
+            mov edx,DWORD[villageSize]
+            sub edx,1
+            cmp ecx,edx
+            je tryNextFarmSpot
+
+            cmp BYTE[eax + ecx],'_' ; Check for available village space
             je addFarmToVillage
 
+            tryNextFarmSpot:        ; Move to next village space
             add ecx,1
-            cmp ecx,[villageSize]
+            cmp ecx,DWORD[villageSize]
             jl findFarm
 
             mov eax,DWORD[notEnoughVillageSpace]    ; Error message = 3
             jmp return
 
         
-        addFarmToVillage:
-        mov BYTE[eax + ecx],'F'         ; Add 'F' to village
+        addFarmToVillage:               ; When a farm can successfully be added 
+        mov BYTE[eax + ecx],'f'         ; Add 'f' to village to represent farm in progress
         sub DWORD[villagers + 4], 1     ; Subtract 1 from available villagers
-        add DWORD[farms],1
+        add DWORD[farms],1              ; Add to number of farms
+
+
+        mov ecx,DWORD[farms]            ; Update corresponding farmPtr value
+        sub ecx,1                       ; Index starts at 0, not 1
+        mov edi,DWORD[farmPtr]          ; In order to get pointer to pointer
+        add DWORD[edi+ecx*4],1          ; Start building farm
 
         mov eax,0
         jmp return
-
     return:
 ret
 
 ; **************** Wall Functions ********************
 buildWall:
-    mov eax, [esp+4] ; village pointer
+    mov edi, [esp+4] ; village pointer
 
-    sub DWORD[funds],1 ; cost of wall
+    cmp DWORD[funds],25
+    mov eax,DWORD[notEnoughFunds]
+    jl wallsBuilt
+
+    cmp DWORD[walls],4
+    mov eax,DWORD[wallMaxLevel]
+    je wallsBuilt
+
+    mov eax,0
+
+    sub DWORD[funds],25 ; cost of wall
     add DWORD[walls],1
 
-    mov BYTE[eax],'W'           ; Add 'W' to village
+        mov BYTE[edi],'w'
+        
+        cmp DWORD[walls],2
+        jl wallsBuilt
+
+        mov BYTE[edi],'W'
+        cmp DWORD[walls],3
+        jl wallsBuilt
+
+        mov edx,DWORD[villageSize]
+        sub edx,1
+        mov BYTE[edi+edx],'w'
+        cmp DWORD[walls],4
+        jl wallsBuilt
+
+        mov BYTE[edi+edx],'W'           ; Add 'W' to village
+    wallsBuilt:
+ret
+
+getWalls:
+    mov eax,DWORD[walls]
 ret
 
 ;****************** Time *****************************
 moveTime:
+    mov eax, [esp+4] ; village pointer
     mov ecx, 0
+    mov edi,DWORD[farmPtr]
     farmLoop:
+        cmp DWORD[edi+ecx*4],1
+        jne noFundsAdded
         add DWORD[funds],1      ; Add 1 to funds for each farm
                                 ; Amount to add subject to change
 
+        noFundsAdded:
         add ecx, 1
         cmp ecx, DWORD[farms]
         jl farmLoop
+    
+    mov edx,DWORD[timeToMakeFarm]
+    mov ecx, 0
+    upgradeFarms:
+        cmp DWORD[edi+ecx*4],edx
+        jl upgradeFarmsDone
+        cmp DWORD[edi+ecx*4],1
+        je upgradeFarmsNextLoop
+        
+        add DWORD[edi+ecx*4],1
+
+        cmp DWORD[edi+ecx*4],1
+        jne upgradeFarmsNextLoop
+        add ecx,1
+        mov BYTE[eax+ecx],'F'
+        sub ecx,1
+
+        upgradeFarmsNextLoop:
+            add ecx,1
+            cmp ecx,DWORD[numPossibleFarms]
+            jl upgradeFarms
+        upgradeFarmsDone:
+
+    add DWORD[hour],1
+    cmp DWORD[hour],24
+    jl dontResetTime
+    mov DWORD[hour],0
+
+    cmp DWORD[attackChancePerNight],100
+    je dontResetTime
+    add DWORD[attackChancePerNight],10
+    dontResetTime:
+ret
+
+getTime:
+    mov eax,DWORD[hour]
+ret
+
+getAttackChance:
+    mov eax,DWORD[attackChancePerNight]
+ret
+
+attack:
+    mov edi, [esp+4] ; village pointer
+    mov eax,0
+    cmp DWORD[walls],0
+    je gameOver
+
+        sub DWORD[walls],1
+        mov edx,DWORD[villageSize]
+        sub edx,1
+        mov BYTE[edi+edx],'w'
+        
+        cmp DWORD[walls],3
+        je attackReturn
+
+        mov BYTE[edi+edx],'_'
+        cmp DWORD[walls],2
+        je attackReturn
+
+        mov BYTE[edi],'w'
+        cmp DWORD[walls],1
+        je attackReturn
+
+        mov BYTE[edi],'_'
+        jmp attackReturn
+
+    gameOver:
+    mov eax,DWORD[gameOverMessage]
+    attackReturn:
 ret
 
 ; **************** Funds Functions ********************
-addFunds:
-    add DWORD[funds],1
-ret
-
 getFunds:
     mov eax, DWORD[funds]
 ret
@@ -162,6 +319,12 @@ notEnoughVillagers:
     dd 2
 notEnoughVillageSpace:
     dd 3
+gameOverMessage:
+    dd 5
+notEnoughFunds:
+    dd 6
+wallMaxLevel:
+    dd 7
 
 section .data
 villagers: 
@@ -174,5 +337,20 @@ walls:
 farms:
     dd 0
 
+numPossibleFarms:
+    dd 0
+
+timeToMakeFarm:
+    dd -3
+
+farmPtr:
+    dd 0        ; Pointer to array with farm information
+
 funds:
-    dd 100
+    dd 10
+
+hour:
+    dd 9
+
+attackChancePerNight:
+    dd 0
